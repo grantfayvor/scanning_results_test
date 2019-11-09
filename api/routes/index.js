0ba63064f4be14db33008ad70960a8fecbde164d
Harrison Favour <fayvorharrison101@gmail.com>
2019-11-09 15:52:38 +0100

    implemented functionality for storing and retrieving scan results

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'),
+  router = express.Router(),
+  resultStore = require('../stores/ResultStore');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  try {
+    let results = await resultStore.getAll();
+    res.json({
+      data: {
+        items: results,
+        total: results.length
+      }
+    });
+  } catch (err) {
+    res.status(500).send({ message: "An error occurred", err });
+  }
+});
+
+router.post('/', async function (req, res, next) {
+  try {
+    let result = await resultStore.save(req.body.result);
+    res.json({
+      data: result
+    });
+  } catch (err) {
+    res.status(500).send({ message: "An error occurred", err });
+  }
 });
 
 module.exports = router;
---
+++ api/stores/ResultStore.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema;
+
+const ResultSchema = new Schema({
+  status: {
+    type: String,
+    required: true,
+    enum: ["Queued", "In Progress", "Success", "Failure"]
+  },
+  repositoryName: {
+    type: String,
+    required: true
+  },
+  findings: [mongoose.SchemaTypes.Mixed],
+  queuedAt: {
+    type: Date,
+    required: true
+  },
+  scanningAt: {
+    type: Date,
+    required: true
+  },
+  finishedAt: {
+    type: Date,
+    required: true
+  }
+});
+
+const model = mongoose.model("Result", ResultSchema);
+
+function ResultStore() {
+
+  this.save = function (result) {
+    return model.create(result);
+  };
+
+  this.getAll = function () {
+    return model.find();
+  };
+}
+
+module.exports = new ResultStore();
