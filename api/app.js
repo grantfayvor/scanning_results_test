0168aaf064e2680dd9a17d45d0663244fcf9d5f8
Harrison Favour <fayvorharrison101@gmail.com>
2019-11-09 15:26:33 +0100

    setup dotenv to manage environment variables

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors'),
+  express = require('express'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  logger = require('morgan'),
+  cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 
