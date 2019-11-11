32da324a20470534df9489b86a29f57e0c1d5017
Harrison Favour <fayvorharrison101@gmail.com>
2019-11-11 11:15:18 +0100

    change search placeholder to repository name

--- dashboard/src/components/NavBar.jsx
+++ dashboard/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 const NavBar = props => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">GuardsRails.io</Navbar.Brand>
+      <Navbar.Brand href="/">GuardsRails.io</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
@@ -12,7 +12,7 @@ const NavBar = props => {
           <Nav.Link href="/CreateScanResult">Create Scan Result</Nav.Link>
         </Nav>
         <Form inline>
-          <FormControl type="text" placeholder="Search" defaultValue={props.searchText} onChange={e => props.setSearchText(e.target.value)} className="mr-sm-2" />
+          <FormControl type="text" placeholder="Repository Name" defaultValue={props.searchText} onChange={e => props.setSearchText(e.target.value)} className="mr-sm-2" />
           <Button variant="outline-success" onClick={props.search}>Search</Button>
         </Form>
       </Navbar.Collapse>
